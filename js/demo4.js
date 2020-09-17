532081338cb90304ce81f37bc72ddd3fd237b612
唐玉薇 <68338617+Tang-v@users.noreply.github.com>
2020-09-17 09:28:10 +0800

    Add files via upload

---
+++ js/demo4.js
@@ -0,0 +1,12 @@
+$(function() {
+	$(window).scroll(function() {
+		var topp = $(document).scrollTop();
+		if (topp > 800) {
+			$(".header").addClass("head_b");
+			$(".navbar-default .navbar-nav>li>a").addClass("head_b");
+		} else {
+			$(".header").removeClass("head_b");
+			$(".navbar-default .navbar-nav>li>a").removeClass("head_b");
+		}
+	})
+})
